6b83b5b49104149ac201130ed8900486fcd14f1f
Abdirahman Abdi <abdirahmanm2022@gmail.com>
2025-12-17 19:13:11 +0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+require('datejs');
+function combineUsers(...args) {
+  
+  const combinedObject = {
+    users: []
+  };
+
+  for (let userArray of args) {
+    combinedObject.users.push(...userArray);
+  }
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
+
+const combineUsers = require('./combineUsers');
+
+const siteA = ['alice', 'bob'];
+const siteB = ['charlie', 'diana'];
+const siteC = ['eve'];
+
+console.log(combineUsers(siteA, siteB, siteC));
+
 
 
 
